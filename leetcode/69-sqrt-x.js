1f6baec40998e0ab984c17033bcd3e0643d10778
Anggi Priatmadi <anggi.arlandi@gmail.com>
2018-01-31 15:43:32 -0500

    solve leetcode #69

---
+++ leetcode/69-sqrt-x.js
@@ -0,0 +1,49 @@
+/*
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+x is guaranteed to be a non-negative integer.
+*/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+// O(sqrt(n)) Solution
+var mySqrt = function(x) {
+    if (x === 0 || x === 1) return x;
+
+    let i = 1;
+    while (i*i < x) {
+        i++;
+    }
+
+    return i*i === x ? i : i - 1;
+};
+
+// Binary Search solution - O(n)
+var mySqrt = function(x) {
+    if (x < 2) return x;
+
+    let start = 0;
+    let end = x;
+    let answer = start;
+
+    while (start <= end) {
+        const mid = Math.floor((start + end) / 2);
+        const mid2 = mid * mid;
+
+        if (mid2 === x) return mid;
+
+        if (mid2 > x) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+            answer = mid;
+        }
+    }
+
+    return answer;
+};
